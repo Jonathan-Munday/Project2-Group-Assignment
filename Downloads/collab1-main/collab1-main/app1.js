508ba4b4a58211a80534798a2daa30a074dfaf3e
Sinovuyo007 <144615989+Sinovuyo007@users.noreply.github.com>
2023-09-22 09:19:45 +0200

    README

---
+++ Downloads/collab1-main/collab1-main/app1.js
@@ -0,0 +1,26 @@
+
+// names ans password
+const loginCredentials = {
+    "julie": "iamjulie",
+    "sanders": "kfc",
+    "login-bot": "not-a-bot"
+}
+
+
+function checkCredentials() {
+    let name = document.forms["login-form"]["username"].value
+    let password = document.forms["login-form"]["password"].value
+
+    console.log(name)
+    console.log(password)
+
+    // console.log(loginCredentials[name])
+    if (loginCredentials[name] != undefined) {
+        window.alert(true)
+        window.open("./404.html")
+    }
+
+    else {
+        window.alert(loginCredentials[name])
+    }
+}
