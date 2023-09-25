8d106af8194ef54fb42004005c287f27095c53d5
Sinovuyo007 <144615989+Sinovuyo007@users.noreply.github.com>
2023-09-25 18:17:44 +0200

    Added onto Index,Contact and About

---
+++ Downloads/collab1-br-sino/collab1-br-sino/README.md
@@ -0,0 +1,16 @@
+# First github collaboration
+> Project name:   *Github-collab*
+
+- desc:         A simple web-project to get used to using git and github
+                to collaborate on a project and use repositories
+- started:      09.09.23
+- updated       09.09.23
+
+- status:       work-in-progess; 
+                using git? yes
+                connected to github? yes
+
+*more notes*
+============
+[empty]
+<end>
---
+++ Downloads/collab1-br-sino/collab1-br-sino/scripts/login.js
@@ -0,0 +1,29 @@
+
+// names ans password
+const loginCredentials = {
+    "julie": "iamjulie",
+    "sanders": "kfc",
+    "login-bot": "not-a-bot"
+}
+
+
+function checkCredentials() {
+    let name = document.forms["login-form"]["username"].value
+    let password = document.forms["login-form"]["password"].value
+
+    console.log(name)
+    console.log(password)
+
+    // console.log(loginCredentials[name])
+    if (loginCredentials[name] != undefined) {
+        window.alert(true)
+        window.open("./")
+        window.open("./success.html")
+        location.assign("https://www.google.com")
+        window.open("./404.html")
+    }
+
+    else {
+        window.alert(loginCredentials[name])
+    }
+}
